a10065f46621f225f2ea7dc53742b1a8fa7aed79
mura651 <51842244+mura651@users.noreply.github.com>
2024-12-02 22:43:06 +0900

    Update main.go

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// hello worldという文字列をレスポンスとして返す
-	fmt.Fprintf(w, "API接続テストが成功しました")
+	fmt.Fprintf(w, "API接続テストが成功しました_ver2")
 }
 
 func TestHandler(w http.ResponseWriter, r *http.Request) {
