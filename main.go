11af1f0f3219f6bc96049e94969bc3dab9dd9a3c
mura651 <pivotal6051@gmail.com>
2024-12-08 21:19:02 +0900

    20241208_ver4

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// hello worldという文字列をレスポンスとして返す
-	fmt.Fprintf(w, "API接続テストが成功しました_20241208_ver3")
+	fmt.Fprintf(w, "API接続テストが成功しました_20241208_ver4")
 }
 
 func TestHandler(w http.ResponseWriter, r *http.Request) {
